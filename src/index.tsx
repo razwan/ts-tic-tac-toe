9efb5e03b97b79507ebbc810179f70765bf4afa3
Razvan Onofrei <razwan.me@gmail.com>
2022-12-21 10:04:09 +0200

    use createRoot

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import { App } from './components';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+const root = createRoot( document.getElementById( 'root' ) );
+
+root.render( <App /> );
